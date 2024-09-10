9f5afcd54a97e18ef2f93b86b22ee1c8b355ba9f
comsaziz <comsaziz@gmail.com>
2024-09-10 11:16:14 +0300

    add object

---
+++ main.js
@@ -0,0 +1,42 @@
+const User  = {
+name: "Aziz",
+id: 20,
+email: "comsaziz2@g",
+gender: "male",
+Availab: true
+
+address:{
+add1:"Malqa",
+add2:"Qeblatien",
+add3:"Monissiyah",
+
+  city:{
+ci:"Madina",
+ci2:"Riyadh",
+
+arr:{
+list:["JS,Flutter,Nodejs,expressjs"],
+
+function:Tuwaiq()
+
+}
+ 
+
+}
+  }
+
+}
+
+
+}
+
+
+console.log(User)
+console.log(User.name)
+User.id = 24
+console.log(User.id)
+delete User.email
+console.log(User.email)
+console.log(User.address)
+console.log(User.address.city.ci1)
+// console.log(User.address.city.arr.list[2])
